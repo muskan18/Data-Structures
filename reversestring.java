55d66c16b353d5aa66144fb05a136b1e60dade6d
Muskan Bansal <44235014+muskan18@users.noreply.github.com>
2019-11-14 22:35:57 +0530

    Update reversestring.java

--- reversestring.java
+++ reversestring.java
@@ -19,47 +19,54 @@ public class reversestring {
 	
 	static void reverse(String str)
 	{
+		char ch[] = str.toCharArray();
+	
+		int q=0;
+
+		int w=ch.length-1;
+
+		while(q<w) {
+
+			while(!((ch[q]>='A'&&ch[q]<='Z')||(ch[q]>='a'&&ch[q]<='z')) && q<w) 
+
+			{
+
+				q++;
+
+			}
+
+			while(!((ch[w]>='A'&&ch[w]<='Z')||(ch[w]>='a'&&ch[w]<='z')) && w>q) 
+
+			{
+
+				w--;
+
+			}
+
+			if(q<w) {
+
+				char c=ch[q];
+
+				ch[q]=ch[w];
+
+				ch[w]=c;
+
+				q++;
+
+				w--;
+
+			}
+
+			
+
+		}
+
+		str=new String(ch);
+
+		System.out.println("The reversed string is " + str);
 		
-        char arr[] = new char[str.length()];
-        
-        for (int i = 0; i < str.length(); i++) {
-        
-        	if (str.charAt(i) < 48 || (str.charAt(i) > 57 && str.charAt(i) < 65) || (str.charAt(i) > 90 && str.charAt(i) < 97) || str.charAt(i) > 122)
-                arr[i] = str.charAt(i);
-            else
-                arr[i] = '0';
-        }
-        
-        Stack<Character> stack = new Stack<>();
-        
-        for (int i = 0; i < str.length(); i++) {
-        
-        	stack.push(str.charAt(i));
-        }
-        
-        
-        int i=0;
-        
-        while(!stack.isEmpty()){
-        
-        	char pop = stack.pop();
-            
-        	if (!(pop < 48 || (pop > 57 && pop < 65) || (pop > 90 && pop < 97) || pop > 122)){
-                arr[i] = pop;
-                ++i;
+		
             }
-            
-        	if(arr[i]!='0'){
-                ++i;
-            }
-        }
-        
-        
-        for ( i = 0; i < str.length(); i++) {
-        
-        	System.out.print(arr[i]);
-        }
-    }
 		
 	
 }
